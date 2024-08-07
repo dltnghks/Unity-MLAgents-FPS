cc4e6746bb6d35bc99ed18fbf14108de00548bc2
LeeSooHwan <dltrnghks@naver.com>
2024-08-07 14:48:32 +0900

    Feat : GameManager Phase Add 함수

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,6 +6,12 @@ using UnityEngine.Diagnostics;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance = null;
+
+    private float _playTime;
+    private int _gamePhase = 0;
+    private List<float> _phaseClearTimeList = new List<float>();
+    public bool IsTrainning;
+
     public static GameManager Instance
     {
         get { return _instance; }
@@ -14,6 +20,25 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         _instance = this;
+        Init();
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Init()
+    {
+        _gamePhase = 0;
+        _playTime = 0;
+        _phaseClearTimeList.Clear();
+    }
+
+    private void Update()
+    {
+        _playTime += Time.deltaTime;
+    }
+
+    public void AddGamePhase()
+    {
+        _gamePhase++;
+        _phaseClearTimeList.Add(_playTime);
     }
 }
