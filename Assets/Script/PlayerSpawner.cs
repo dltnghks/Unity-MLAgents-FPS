2f47076a6f2f64429c97e07dcd5e4361ea9ff483
LeeSooHwan <dltrnghks@naver.com>
2024-08-07 14:09:14 +0900

    Refactor : Spawner 스크립트 파일 이름 변경

--- Assets/Script/PlayerSpawner.cs
+++ Assets/Script/PlayerSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpanwPoint : MonoBehaviour
+public class PlayerSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
