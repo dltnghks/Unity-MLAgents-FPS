d3f6310d965c0376d28499197fd48da2e3c6c503
LeeSooHwan <dltrnghks@naver.com>
2024-08-07 14:47:38 +0900

    Feat : Object Spawner

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> spawnPointList = new List<GameObject>();
+    public GameObject spawnObject;
+
+    private void Start()
+    {
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            spawnPointList.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    protected GameObject InstantiateObject()
+    {
+        var returnObject = Instantiate(spawnObject);
+        returnObject.transform.SetParent(this.transform);
+        return returnObject;
+    }
+
+    public void OnePointRandomSpawn()
+    {
+        var spawnedObject = InstantiateObject();
+        int pointIndex = Random.Range(0, spawnPointList.Count);
+        Vector3 position = spawnPointList[pointIndex].transform.position;
+        spawnedObject.transform.position = position;
+    }
+
+    public void OnePointSpawn(Vector3 position, Quaternion rotation)
+    {
+        var spawnedObject = InstantiateObject();
+        spawnedObject.transform.SetPositionAndRotation(position, rotation);
+    }
+}
